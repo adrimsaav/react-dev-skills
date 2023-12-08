b86231b2eee918e4f579617aa105ed72ad31e13a
adrimsaav <adrimsaav@gmail.com>
2023-12-08 15:47:56 -0500

    finish part 3

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import NewSkillForm from "./NewSkillForm";
 import SkillList from "./SkillList";
 
 export default function App() {
-  const [ skillsData, setSkills ] = useState([
+  const [ skills, setSkills ] = useState([
       { name: "HTML", level: 5 },
       { name: "CSS", level: 3 },
       { name: "JavaScript", level: 4 },
       { name: "Python", level: 2 },
   ]);
+
+  const [showSkills] = useState(true)
   
   return (
     <div className="App">
       <h1>React Dev Skills</h1>
-      <SkillList skillsData={skillsData} />
-      <NewSkillForm />
+      { showSkills && <SkillList skillsData={skills} />}
+          <NewSkillForm />
     </div>
   );
 }
